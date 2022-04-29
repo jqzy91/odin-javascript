d2c113a66e7c7bffae1c2b70dce785f49eabc1fd
jqzy91 <jamesquek91@live.com.sg>
2022-04-29 17:23:23 +0800

    revisiting rps 01

--- odin-rps.js
+++ odin-rps.js
@@ -34,83 +34,83 @@ function roundPlay(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let drawScore = 0;
-  for (let i = 1; i <= 5; i++) {
-    const playerSelection = prompt("Choose Rock, Paper or Scissors.", "Rock");
-    if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Rock" beats "Scissors", you won!`
-    ) {
-      playerScore += 1;
-      console.log(`"Rock" beats "Scissors", you won!`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Paper" beats "Rock", you won!`
-    ) {
-      playerScore += 1;
-      console.log(`"Paper" beats "Rock", you won!`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Scissors" beat "Paper", you won!`
-    ) {
-      playerScore += 1;
-      console.log(`"Scissors" beats "Paper", you won!`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Rock" and "Rock", it's a draw.`
-    ) {
-      drawScore += 1;
-      console.log(`"Rock" and "Rock", it's a draw.`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Paper" and "Paper", it's a draw.`
-    ) {
-      drawScore += 1;
-      console.log(`"Paper" and "Paper", it's a draw.`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Scissors" and "Scissors", it's a draw.`
-    ) {
-      drawScore += 1;
-      console.log(`"Scissors" and "Scissors", it's a draw.`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Rock" beats "Scissors", you lost.`
-    ) {
-      computerScore += 1;
-      console.log(`"Rock" beats "Scissors", you lost.`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Paper" beats "Rock", you lost.`
-    ) {
-      computerScore += 1;
-      console.log(`"Paper" beats "Rock", you lost.`);
-    } else if (
-      roundPlay(playerSelection, computerPlay()) ===
-      `"Scissors" beat "Paper", you lost.`
-    ) {
-      computerScore += 1;
-      console.log(`"Scissors" beat "Paper", you lost.`);
-    } else {
-      i--;
-      console.log("Error! Try again!");
-    }
-    console.log("Player's Score: " + playerScore);
-    console.log("Computer's Score: " + computerScore);
-    console.log("Rounds Draw: " + drawScore);
-    console.log("Rounds Played: " + i);
-  }
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   let drawScore = 0;
+//   for (let i = 1; i <= 5; i++) {
+//     const playerSelection = prompt("Choose Rock, Paper or Scissors.", "Rock");
+//     if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Rock" beats "Scissors", you won!`
+//     ) {
+//       playerScore += 1;
+//       console.log(`"Rock" beats "Scissors", you won!`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Paper" beats "Rock", you won!`
+//     ) {
+//       playerScore += 1;
+//       console.log(`"Paper" beats "Rock", you won!`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Scissors" beat "Paper", you won!`
+//     ) {
+//       playerScore += 1;
+//       console.log(`"Scissors" beats "Paper", you won!`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Rock" and "Rock", it's a draw.`
+//     ) {
+//       drawScore += 1;
+//       console.log(`"Rock" and "Rock", it's a draw.`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Paper" and "Paper", it's a draw.`
+//     ) {
+//       drawScore += 1;
+//       console.log(`"Paper" and "Paper", it's a draw.`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Scissors" and "Scissors", it's a draw.`
+//     ) {
+//       drawScore += 1;
+//       console.log(`"Scissors" and "Scissors", it's a draw.`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Rock" beats "Scissors", you lost.`
+//     ) {
+//       computerScore += 1;
+//       console.log(`"Rock" beats "Scissors", you lost.`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Paper" beats "Rock", you lost.`
+//     ) {
+//       computerScore += 1;
+//       console.log(`"Paper" beats "Rock", you lost.`);
+//     } else if (
+//       roundPlay(playerSelection, computerPlay()) ===
+//       `"Scissors" beat "Paper", you lost.`
+//     ) {
+//       computerScore += 1;
+//       console.log(`"Scissors" beat "Paper", you lost.`);
+//     } else {
+//       i--;
+//       console.log("Error! Try again!");
+//     }
+//     console.log("Player's Score: " + playerScore);
+//     console.log("Computer's Score: " + computerScore);
+//     console.log("Rounds Draw: " + drawScore);
+//     console.log("Rounds Played: " + i);
+//   }
 
-  if ((i = 5 && playerScore > computerScore)) {
-    console.log("Congratulations, you achieved total victory!");
-  } else if ((i = 5 && playerScore < computerScore)) {
-    console.log("Unfortunately, you suffered total defeat!");
-  } else if ((i = 5 && playerScore === computerScore)) {
-    console.log("It is a stalemate!");
-  }
-}
+//   if ((i = 5 && playerScore > computerScore)) {
+//     console.log("Congratulations, you achieved total victory!");
+//   } else if ((i = 5 && playerScore < computerScore)) {
+//     console.log("Unfortunately, you suffered total defeat!");
+//   } else if ((i = 5 && playerScore === computerScore)) {
+//     console.log("It is a stalemate!");
+//   }
+// }
 
-game();
+// game();
